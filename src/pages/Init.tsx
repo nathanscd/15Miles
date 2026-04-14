0a1c1ab3a50b220a7f4efc7de4fec732c9eaa5b7
nathanscd <nathansec26@gmail.com>
2026-04-14 09:38:04 -0300

    Init

---
+++ src/pages/Init.tsx
@@ -0,0 +1,14 @@
+import './style/Init.css'
+
+const Init = () => {
+  return (
+    <div className="init-wrapper">
+      <div className="initial-section">
+        <img src="/logo.svg" alt="Logo" />
+        <h1>Transformando Leads em vendas.</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Init
