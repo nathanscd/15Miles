30f072c0078df3c34c7a4640d299cc9ad9c02244
nathanscd <nathansec26@gmail.com>
2026-05-04 10:36:15 -0300

    Style change... again...

--- src/pages/Init.tsx
+++ src/pages/Init.tsx
@@ -1,15 +1,44 @@
 import './style/Init.css'
-import Header from '../components/Header'
 
 const Init = () => {
   return (
     <div className="init-wrapper">
-      <Header />
-      <div className="initial-section"></div>
+      <section className='home'>
+        <div className='left'>
+          <img src="15Miles.svg" alt="" />
+          <h1>Seja bem vindo à 15Miles</h1>
+          <h3>A melhor experiência que o seu negócio poderia ter!</h3>
+          <div className='mini-form'>
+            <input type="text" placeholder="Digite seu email" />
+            <button>Comunicar</button> 
+          </div>
+        </div>
 
-      <div className='presentation'>
-        <h1>Always <img src="/logo.svg" /> ahead </h1>
-      </div>
+        <div className='right'>
+          <div className='best-project'>
+            <h1>Nosso projeto destaque</h1>
+            <a href="">Ver mais</a>
+          </div>
+
+          <div className='social-media'>
+            <a href="https://www.facebook.com/15miles" target="_blank" rel="noopener noreferrer">
+              <img src="facebook.svg" alt="Facebook" />
+            </a>
+            <a href="https://www.instagram.com/15miles" target="_blank" rel="noopener noreferrer">
+              <img src="instagram.svg" alt="Instagram" />
+            </a>
+            <a href="https://www.twitter.com/15miles" target="_blank" rel="noopener noreferrer">
+              <img src="twitter.svg" alt="Twitter" />
+            </a>
+          </div>
+
+          <div className='responsible'>
+            <h1>Responsável</h1>
+            <img src="nathanael.jpg" alt="Nathanael Secundo Cardoso" />
+            <h3>Nathanael Secundo Cardoso</h3>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
